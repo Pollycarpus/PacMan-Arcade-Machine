c8e98a98d8bede650e7e4dcf506166a0be4d38c9
Frankuy <fhizbullah99@gmail.com>
2019-05-18 21:52:17 +0700

    Add maze and assets display

---
+++ display.js
@@ -0,0 +1,76 @@
+$(".wall").css(
+    {
+        "width":"20px",
+        "height":"20px",
+        "display":"inline-block",
+        // "background-image":"url(\"assets/coin.gif\")", 
+        "background-repeat":"no-repeat",
+        "background-position":"center",
+        "background-color":"darkblue",
+        "border" : "2px solid purple",
+        "margin" : "-2px",
+    }
+);
+
+$(".coin").css(
+    {
+        "width":"20px",
+        "height":"20px",
+        "display":"inline-block",
+        "background-image":"url(\"assets/coin.gif\")", 
+        "background-repeat":"no-repeat",
+        "background-position":"center",
+        "background-color":"black",
+    }
+);
+
+$(".ground").css(
+    {
+        "width":"20px",
+        "height":"20px",
+        "display":"inline-block",
+        // "background-image":"url(\"assets/coin.gif\")", 
+        "background-repeat":"no-repeat",
+        "background-position":"center",
+        "background-color":"black",
+    }
+);
+
+$(".pacman").css(
+    {
+        "width":"20px",
+        "height":"20px",
+        "display":"inline-block",
+        "background-image":"url(\"assets/pacman.gif\")", 
+        "background-repeat":"no-repeat",
+        "background-position":"center",
+        "background-color":"black",
+        "background-size":"cover",
+    }
+);
+
+$(".ghost").css(
+    {
+        "width":"20px",
+        "height":"20px",
+        "display":"inline-block",
+        "background-image":"url(\"assets/ghost.gif\")", 
+        "background-repeat":"no-repeat",
+        "background-position":"center",
+        "background-color":"black",
+        "background-size":"cover",
+    }
+);
+
+$(".cherries").css(
+    {
+        "width":"20px",
+        "height":"20px",
+        "display":"inline-block",
+        "background-image":"url(\"assets/cherries.png\")", 
+        "background-repeat":"no-repeat",
+        "background-position":"center",
+        "background-color":"black",
+        "background-size":"cover",
+    }
+);
